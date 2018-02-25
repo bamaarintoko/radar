04a6aca845d5c7e3829032983be772ee6ff0ee79
Yosafat Bama <yosafatbama.arintoko@gmail.com>
2018-02-25 15:10:57 +0700

    get latitude and longitude

--- App.js
+++ App.js
@@ -4,55 +4,77 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+    Dimensions
 } from 'react-native';
+import MapView from 'react-native-maps';
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
+    ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
+    android: 'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
-
+let {width, height} = Dimensions.get('window')
+const styles = StyleSheet.create({
+    container: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    map: {
+        width: width,
+        height: height
+    },
+});
 type Props = {};
 export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
-}
+    constructor(props) {
+        super(props);
+        this.state = {
+            latitude : 0,
+            longitude : 0
+        }
+    }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+
+    componentDidMount() {
+        console.log('asu')
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                console.log("--->",position.coords.latitude)
+                console.log("--->",position.coords.longitude)
+            })
+    }
+
+
+    render() {
+        const { region } = this.props;
+        //console.log(region);
+        return (
+            <View style={styles.container}>
+                <View style ={styles.container}>
+                    <MapView
+                        style={styles.map}
+                        region={{
+                            latitude: 37.78825,
+                            longitude: -122.4324,
+                            latitudeDelta: 0.015,
+                            longitudeDelta: 0.0121,
+                        }}
+                    >
+                    </MapView>
+                </View>
+            </View>
+        );
+    }
+}
